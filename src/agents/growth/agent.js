b50886098b673011bcc1e4f54ee10ac3d94e3f11
Lakkygen <michaeladedeji366@gmail.com>
2026-09-02 19:09:46 +0100

    Implement GrowthAgent class with system prompt and tools

---
+++ src/agents/growth/agent.js
@@ -0,0 +1,22 @@
+// src/agents/growth/agent.js
+
+import { Agent } from "../base/Agent.js";
+import { GROWTH_SYSTEM_PROMPT } from "./prompt.js";
+import { GROWTH_ALLOWED_TOOLS } from "./tools.js";
+
+export class GrowthAgent extends Agent {
+  constructor(options = {}) {
+    super("growth", options);
+
+    this.systemPrompt = GROWTH_SYSTEM_PROMPT;
+    this.allowedTools = GROWTH_ALLOWED_TOOLS;
+  }
+
+  getSystemPrompt() {
+    return this.systemPrompt;
+  }
+
+  getAllowedTools() {
+    return [...this.allowedTools];
+  }
+}
