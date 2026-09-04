8a71721517c354cc52db43e6babe10ef8ffafd30
Lakkygen <michaeladedeji366@gmail.com>
2026-09-04 14:13:55 +0100

    Fix import path and format SupportAgent properties
    Refactor import statement and format class properties.

--- src/agents/support/agent.js
+++ src/agents/support/agent.js
@@ -1,15 +1,14 @@
-// src/agents/support/agent.js
-
-import { Agent } from "../base/Agent.js";
+import { Agent } from "../base/agent.js";
 import { SUPPORT_SYSTEM_PROMPT } from "./prompt.js";
 import { SUPPORT_ALLOWED_TOOLS } from "./tools.js";
 
 export class SupportAgent extends Agent {
   constructor(options = {}) {
     super("support", options);
-
-    this.systemPrompt = SUPPORT_SYSTEM_PROMPT;
-    this.allowedTools = SUPPORT_ALLOWED_TOOLS;
+    this.systemPrompt =
+      SUPPORT_SYSTEM_PROMPT;
+    this.allowedTools =
+      SUPPORT_ALLOWED_TOOLS;
   }
 
   getSystemPrompt() {
