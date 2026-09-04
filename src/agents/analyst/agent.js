a0b8159e469a69ce66d3b7debb665541bb5e0244
Lakkygen <michaeladedeji366@gmail.com>
2026-09-04 14:41:38 +0100

    Refactor import statement for ANALYST_ALLOWED_TOOLS

--- src/agents/analyst/agent.js
+++ src/agents/analyst/agent.js
@@ -1,14 +1,12 @@
 import { Agent } from "../base/agent.js";
 import { ANALYST_SYSTEM_PROMPT } from "./prompt.js";
-import { ANALYST_ALLOWED_TOOLS } from "./tools.js";
+import { analystTools as ANALYST_ALLOWED_TOOLS } from "./tools.js";
 
 export class AnalystAgent extends Agent {
   constructor(options = {}) {
     super("analyst", options);
-    this.systemPrompt =
-      ANALYST_SYSTEM_PROMPT;
-    this.allowedTools =
-      ANALYST_ALLOWED_TOOLS;
+    this.systemPrompt = ANALYST_SYSTEM_PROMPT;
+    this.allowedTools = ANALYST_ALLOWED_TOOLS;
   }
 
   getSystemPrompt() {
