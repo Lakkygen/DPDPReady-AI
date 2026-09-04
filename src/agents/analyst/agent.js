edf2bc9c5927c909d8a5ecffda77d026ad0bf25b
Lakkygen <michaeladedeji366@gmail.com>
2026-09-04 14:13:12 +0100

    Fix import case and reformat AnalystAgent class
    Refactor import statement and format class properties.

--- src/agents/analyst/agent.js
+++ src/agents/analyst/agent.js
@@ -1,15 +1,14 @@
-// src/agents/analyst/agent.js
-
-import { Agent } from "../base/Agent.js";
+import { Agent } from "../base/agent.js";
 import { ANALYST_SYSTEM_PROMPT } from "./prompt.js";
 import { ANALYST_ALLOWED_TOOLS } from "./tools.js";
 
 export class AnalystAgent extends Agent {
   constructor(options = {}) {
     super("analyst", options);
-
-    this.systemPrompt = ANALYST_SYSTEM_PROMPT;
-    this.allowedTools = ANALYST_ALLOWED_TOOLS;
+    this.systemPrompt =
+      ANALYST_SYSTEM_PROMPT;
+    this.allowedTools =
+      ANALYST_ALLOWED_TOOLS;
   }
 
   getSystemPrompt() {
