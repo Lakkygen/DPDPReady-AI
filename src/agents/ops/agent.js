592d755c6fd0513dea916c087dc14efe75fc234b
Lakkygen <michaeladedeji366@gmail.com>
2026-09-04 14:39:56 +0100

    Define OPS_ALLOWED_TOOLS directly in agent.js

--- src/agents/ops/agent.js
+++ src/agents/ops/agent.js
@@ -1,14 +1,28 @@
 import { Agent } from "../base/agent.js";
 import { OPS_SYSTEM_PROMPT } from "./prompt.js";
-import { OPS_ALLOWED_TOOLS } from "./tools.js";
+
+const OPS_ALLOWED_TOOLS = [
+  "health_check",
+  "website_inspect",
+  "github_repository",
+  "github_get_file",
+  "github_search_files",
+  "github_create_branch",
+  "github_update_file",
+  "github_create_pr",
+  "render_deploy",
+  "render_rollback",
+  "browser_open",
+  "browser_screenshot",
+  "browser_run",
+  "qa_run",
+];
 
 export class OpsAgent extends Agent {
   constructor(options = {}) {
     super("ops", options);
-    this.systemPrompt =
-      OPS_SYSTEM_PROMPT;
-    this.allowedTools =
-      OPS_ALLOWED_TOOLS;
+    this.systemPrompt = OPS_SYSTEM_PROMPT;
+    this.allowedTools = OPS_ALLOWED_TOOLS;
   }
 
   getSystemPrompt() {
